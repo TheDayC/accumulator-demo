e194d5c2a21da0a5d8363c4b8b9f6a49791b29f1
David Connolly <daveconnolly90@gmail.com>
2020-10-10 12:57:16 +0100

    feature/initial-setup (#1)
    * Added react + ts
    
    * Added Sass and redux
    
    * Some minor file changes
    
    * Added eslint
    
    * Eslint set up
    
    * Added prettier and ran write
    
    * Added pre-commit hooks to ensure linting and formatting done before commiting code
    
    * Some linting rule changes and updated errors
    
    * test
    
    * lint test
    
    * Changed lint-staged rules

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'amd': true,
+        'node': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier', 
+        'prettier/@typescript-eslint'
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        '@typescript-eslint/explicit-module-boundary-types': 'error'
+    },
+    'settings': {
+        'react': {
+            'version': '16.13.1'
+        }
+    }
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
+};
+
+export default App;
